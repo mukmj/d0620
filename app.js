e85fd5de016b0ad6f215cc45d9ef0e4d9915dcdd
Harin <teri941227@gmail.com>
2019-06-20 13:32:30 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var http = require('http'); //import랑 비슷
+var fs = require("fs");
+var server = http.createServer(function(req, res) {
+  res.statusCode = 200; //성공했는지 실패했는지 여부(200은 성공)
+  res.setHeader('Content-Type', 'text/html');
+  //http://localhost/index.html : req.url = index.html
+   //node . 을 쓴 위치를 dirname이 가져옴
+   var url = "";
+   if(req.url == "/"){
+     url = "/index.html";
+   }else{
+     url = req.url;
+   }
+  res.end(fs.readFileSync(__dirname + url));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(80, function() { //listen 켜지면
+  console.log(`Server running`);
 });
