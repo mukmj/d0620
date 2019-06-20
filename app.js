6c9b51ed8a27c3d1f6042473f4314783342c683b
Harin <teri941227@gmail.com>
2019-06-20 14:34:10 +0900

    app 수정

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var http = require('http'); //import랑 비슷
 var fs = require("fs");
 var server = http.createServer(function(req, res) {
-  res.statusCode = 200; //성공했는지 실패했는지 여부(200은 성공)
-  res.setHeader('Content-Type', 'text/html');
+   //성공했는지 실패했는지 여부(200은 성공)
+
   //http://localhost/index.html : req.url = index.html
    //node . 을 쓴 위치를 dirname이 가져옴
    var url = "";
    if(req.url == "/"){
      url = "/index.html";
+   }else if(req.url == "/favicon.ico") {
+     return res.writeHead(404);
+   }else if(require("path").extname(req.url) == ".css"){ //확장명찾는기능
+     url = req.url;
+     res.setHeader('Content-Type', 'text/css');
    }else{
      url = req.url;
+     res.setHeader('Content-Type', 'text/html');
    }
-  res.end(fs.readFileSync(__dirname + url));
+   res.statusCode = 200;
+   res.end(fs.readFileSync(__dirname + url));
 });
 
 server.listen(80, function() { //listen 켜지면
